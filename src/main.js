b07dffcdf27117dcb30e2de37ba799ce277c18f7
praathul <44019979+athulpram@users.noreply.github.com>
2019-01-09 10:14:21 +0530

    Initial commit created function to create grid

---
+++ src/main.js
@@ -0,0 +1,30 @@
+const generateGrid = function(gridSize) {
+  let table = document.createElement("table");
+  for (let row = 0; row < gridSize; row++) {
+    table.appendChild(generateRow(row * gridSize, gridSize));
+  }
+  table.setAttribute("id", "table");
+  table.setAttribute("style", "border :1px solid black");
+  document
+    .getElementById("main")
+    .replaceChild(table, document.getElementById("table"));
+};
+
+const generateRow = function(stratCell, gridSize) {
+  let row = document.createElement("tr");
+  for (let cell = stratCell; cell < gridSize + stratCell; cell++) {
+    row.appendChild(generateCell(cell, gridSize));
+  }
+  return row;
+};
+
+const generateCell = function(id) {
+  let cell = document.createElement("td");
+  cell.setAttribute("id", id);
+  cell.setAttribute("class", "groundCell");
+  return cell;
+};
+
+window.onload = function() {
+  generateGrid(5);
+};
